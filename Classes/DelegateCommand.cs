1e50250af0b3a76073b0b4a6942942f515a69735
iskanderror <iskanderror@gmail.com>
2021-04-08 22:34:47 +0500

    Add project files.

---
+++ Classes/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GeoServices.Classes
+{
+  public class DelegateCommand : ICommand
+  {
+    private readonly Action<object> _method;
+
+    public DelegateCommand(Action<object> method)
+    {
+      _method = method;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+      return true;
+    }
+
+    public void Execute(object parameter)
+    {
+      _method.Invoke(parameter);
+    }
+  }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using GeoServices.Classes;
+using Microsoft.Win32;
+
+namespace GeoServices.ViewModels
+{
+  class MainViewModel : NotifiedEntity
+  {
+    #region Fields
+    private string _searchString;
+    private string _logString;
+    private string _geoData;
+
+    private int _counter;
+
+
+    private DelegateCommand _loadDataCommand;
+    private DelegateCommand _saveFileCommand;
+    #endregion
+
+    #region Properties
+    public string SearchString { get => _searchString; set => SetProperty(ref _searchString, value); }
+    public string LogString { get => _logString; set => SetProperty(ref _logString, value); }
+    public string GeoData { get => _geoData; set => SetProperty(ref _geoData, value); }
+
+    
+
+    public DelegateCommand LoadDataCommand => _loadDataCommand ?? (_loadDataCommand = new DelegateCommand(LoadData));
+    public DelegateCommand SaveFileCommand => _saveFileCommand ?? (_saveFileCommand = new DelegateCommand(SaveFile));
+    #endregion
+
+    public MainViewModel()
+    {
+
+    }
+
+    #region Methods
+
+    public void LoadData(object parameter)
+    {
+      LogString += String.Format("Search string {0} \n", SearchString);
+    }
+
+    public void SaveFile(object parameter)
+    {
+      SaveFileDialog saveFileDialog = new SaveFileDialog()
+      {
+        Filter = "text files (*.txt) | *.txt; ",
+        InitialDirectory = Directory.GetCurrentDirectory()
+      };
+      if (saveFileDialog.ShowDialog() == true)
+      {
+        File.WriteAllText(saveFileDialog.FileName, GeoData);
+        LogString += String.Format("File saved \n");
+      }
+      
+    }
+    #endregion
+
+  }
+}
